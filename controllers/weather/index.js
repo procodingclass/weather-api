71bca8dd6e1ec4fd864949e34930b2d1dff82c0f
vishalgddam873 <vishal18@navgurukul.org>
2024-02-12 12:02:03 +0530

    Weather API updated

--- controllers/weather/index.js
+++ controllers/weather/index.js
@@ -9,12 +9,23 @@ exports.getCurrentWeather = async (req, res, next) => {
 				message: 'Latitude and Longitude is required!',
 			});
 		}
+		// Create a new Date object
+		let currentDate = new Date();
+
+		// Get the components of the date
+		let year = currentDate.getFullYear();
+		let month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based, so add 1
+		let day = currentDate.getDate().toString().padStart(2, '0');
+
 		// Make axios requests
 		const currentWeather = await axios
 			.get(process.env.WEATHER_API_URL, {
 				params: {
 					query: `${lat},${long}`,
-					access_key: process.env.ACCESS_KEY,
+					days: 1,
+					dt: `${year}-${month}-${day}`,
+					lang: 'en',
+					key: process.env.ACCESS_KEY,
 				},
 			})
 			.then((response) => {
@@ -26,8 +37,10 @@ exports.getCurrentWeather = async (req, res, next) => {
 
 		let { location, current } = currentWeather;
 		let weather = {
-			...location,
 			...current,
+			...location,
+			temperature: current?.temp_c,
+			weather_descriptions: [current?.condition?.text],
 		};
 		return res.status(200).json({
 			success: true,
